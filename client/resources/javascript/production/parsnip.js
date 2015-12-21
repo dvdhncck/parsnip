d7ec5378b8cd4dc4db8ffea15d4e9ad7d64e9159
david hancock <mail@davidhancock.com>
2015-12-21 18:13:26 +0000

    basic itemslide with retrieved URLs (from hardcoded search term)

--- client/resources/javascript/production/parsnip.js
+++ client/resources/javascript/production/parsnip.js
@@ -1,8 +1,36 @@
 
+function buildCarousel(imagePaths)
+{
+  var list=$('div#scrolling ul');
+  list.empty();
+  $.each(imagePaths.paths, function(index, path) {
+    //var image = $('img').attr({src:"photos/'" + path + "'", width:"50%"});
+    //var imagePane = $('li');
+    //imagePane.append(image)
+    //list.append(imagePane);
+    var realPath = '/photos/' + path;
+    list.append('<li><img src="' + realPath + '" width="400"></li>');
+  });
+
+  initCarousel();
+}
+
+function initCarousel()
+{
+  carousel = $("ul");
+  carousel.itemslide();
+}
+
 $(function(){
 
- carousel = $("ul");
- carousel.itemslide();
+  var searchPath='2009/2009-04-16-luftwaffenmuseum';
+
+  $.ajax({
+    url: 'path/' + searchPath,
+    data: '',
+    success: buildCarousel,
+    dataType: 'json'
+  });
 
 })
 
